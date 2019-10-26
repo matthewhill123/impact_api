604f538b3908039d4bfac265e62fcf34bfd69e6a
Kevin Lemagnen <kevin@cambridgespark.com>
2019-10-26 15:10:15 +0100

    Fix syntax

--- compute_warming/main.py
+++ compute_warming/main.py
@@ -11,15 +11,15 @@ def compute_warming_from_transport_co2(events_co2):
     """
 
 
-   carbon_equiv_total = sum(events_co2) # for an array of input carbon_equivs
-   if (carbon_equiv_total <= 170308):
-       temp = 1.5
-       carbon_excess = carbon_equiv_total - 88670
-       heating_potential = 6.1E-6
-   else:
-       temp = 2.0
-       carbon_excess = carbon_equiv_total - 170308
-       heating_potential = 3.7E-6
+    carbon_equiv_total = sum(events_co2) # for an array of input carbon_equivs
+    if (carbon_equiv_total <= 170308):
+        temp = 1.5
+        carbon_excess = carbon_equiv_total - 88670
+        heating_potential = 6.1E-6
+    else:
+        temp = 2.0
+        carbon_excess = carbon_equiv_total - 170308
+        heating_potential = 3.7E-6
 
-   total_warming_value = (temp + carbon_excess * heating_potential)
-   return total_warming_value
+    total_warming_value = (temp + carbon_excess * heating_potential)
+    return total_warming_value
