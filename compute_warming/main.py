7af28f4cfad62f3b7f96a5e255da85183d4818f5
matthewhill123 <57003075+matthewhill123@users.noreply.github.com>
2019-10-26 17:17:06 +0100

    Update main.py

--- compute_warming/main.py
+++ compute_warming/main.py
@@ -15,10 +15,10 @@ def compute_warming_from_transport_co2(humanity_CO2_extrapolated):
         total_warming_value = 1.5*(humanity_CO2_extrapolated/651.68E12)
     elif humanity_CO2_extrapolated <= 1251.68E12:
         # 1.5 =< temp =< 2.0  --> temp difference = 0.5
-        total_warming_value = 0.5*(humanity_CO2_extrapolated - 651.68E12)/(1251.68E12-651.68E12)
+        total_warming_value = 0.5*(humanity_CO2_extrapolated - 651.68E12)/(1251.68E12-651.68E12) + 1.5
     else:
         # 2.0 <= temp =< 3.0 --> temp difference = 1
-        total_warming_value = 1.0*(humanity_CO2_extrapolated - 1251.68E12)/(3251.68E12-1251.68E12)
+        total_warming_value = 1.0*(humanity_CO2_extrapolated - 1251.68E12)/(3251.68E12-1251.68E12) + 2.0
 
     return total_warming_value
 
@@ -26,7 +26,7 @@ def compute_warming_from_transport_co2(humanity_CO2_extrapolated):
 # import numpy as np
 # import matplotlib.pyplot as plt
 #
-# test_range = np.arange(0,4000,100)
+# test_range = np.arange(0,4000E12,100E12)
 #
 # test_results = []
 # for i in test_range:
